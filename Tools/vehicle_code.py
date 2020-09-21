1e6d29bae333e1af760c42c030e64901d1c397e3
UmbralReaper <67179462+UmbralReaper@users.noreply.github.com>
2020-09-21 14:34:27 +1000

    Finalise vehicle code as specified in #153
    And also provide a tool, vehicle_code.py, to prevent this from ever becoming an issue again.

---
+++ Tools/vehicle_code.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# Make sure you have python3 installed.
+# Ensure that the json_formatter is kept in Tools with this script.
+# For Windows:
+# Using command prompt type 'python vehicle_code.py'
+# For Max OS X or Linux:
+# Swap any '\\' with '/', then run the script as in windows.
+
+import json
+import os
+
+
+def gen_new(path):
+    change = False
+    with open(path, 'r') as json_file:
+        try:
+            json_data = json.load(json_file)
+        except UnicodeDecodeError:
+            print(f'UnicodeDecodeError in {path}')
+            return None
+        for jo in json_data:
+            locations = {}
+            # We only want JsonObjects
+            if type(jo) is str:
+                continue
+            # And only vehicles
+            if jo.get('type') != 'vehicle':
+                continue
+            for part in jo['parts']:
+                x, y = part['x'], part['y']
+                item = part.get('part')
+                if part.get('parts'):
+                    locations[(x, y)] = {'x':x, 'y':y, 'parts':part['parts']}
+                elif (x, y) in locations:
+                    locations[(x, y)]['parts'].append(item)
+                else:
+                    locations[(x, y)] = {'x':x, 'y':y, 'parts':[item]}
+            jo['parts'] = [locations[key] for key in locations]
+            change = True
+    return json_data if change else None
+
+
+def change_file(json_dir):
+    for root, directories, filenames in os.walk(f'../{json_dir}'):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+            if path.endswith('.json'):
+                new = gen_new(path)
+                if new is not None:
+                    with open(path, 'w') as jf:
+                        json.dump(new, jf, ensure_ascii=False)
+                    os.system(f'.\\json_formatter.exe {path}')
+
+
+if __name__ == "__main__":
+    json_dir = input('What directory is the JSON files in? ')
+    change_file(json_dir)
