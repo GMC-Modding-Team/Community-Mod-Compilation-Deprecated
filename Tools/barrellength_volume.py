94f20fd3a9f5903b00bbefdb85c370a83d67bfd2
UmbralReaper <UmbralReaper@github.com>
2021-02-05 20:18:51 +1000

    Update tools.
    Former-commit-id: 0beb1efd57ebdcfee83aef6b7e9faeb72b84597e [formerly e3f39b2bb1885e8e6b7b860e3db99eef345c4edc]
    Former-commit-id: 01693c917108b5299e3033b13a2cbd7b3856a4da
    Former-commit-id: 037410e021be738ef68f16d9c794331466774f31

--- Tools/barrellength_volume.py
+++ Tools/barrellength_volume.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 
 # Make sure you have python3 installed.
-# Ensure that the json_formatter is kept in Tools with this script.
+# Ensure that the json_formatter is kept in Tools with this script. 
 # They must be in the same folder!
 # For Windows:
 # Using command prompt type "python barrellength_volume.py"
@@ -41,7 +41,7 @@ def gen_new(path):
                     barrel_length = jo["barrel_length"]
                     barrel_length *= 250
                     jo["barrel_volume"] = str(barrel_length) + " ml"
-                if type(jo["barrel_length"]) == str:
+                elif type(jo["barrel_length"]) == str:
                     jo["barrel_volume"] = jo["barrel_length"]
                 del jo["barrel_length"]
                 change = True
